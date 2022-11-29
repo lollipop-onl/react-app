e7b4aa59315493b9e1657b945906361aba7564b9
Ryoya Tamura <lollipop@simochee.net>
2022-11-29 16:58:06 +0900

    🔧 setup tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from '~/App';
+
+import 'tailwindcss/tailwind.css';
+
+document.body.classList.add('font-sans', 'antialiased');
 
 const root = createRoot(document.getElementById('root')!);
 
