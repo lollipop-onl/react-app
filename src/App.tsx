08c2ecaa2467dccf91f2c1404fffadcb854cfdbf
simochee <lollipop@simochee.net>
2024-01-05 17:17:45 +0900

    ⚙️ upgrade deps

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const App: React.FC = () => {
   return (
-    <div className='max-w-screen-md mx-auto px-4 py-10'>
-      <article className='prose'>
+    <div className="mx-auto max-w-screen-md px-4 py-10">
+      <article className="prose">
         <h1>react-app</h1>
         <p>This is a React application template.</p>
         <p>with Vite, TypeScript, TailwindCSS and daisyUI</p>
